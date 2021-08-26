0a7db48d1d1f7dbadb811318f6159cbd5178a946
cutls <web-pro@cutls.com>
2021-08-26 21:33:38 +0900

    test

--- test/test1/interfaces2.ts
+++ test/test1/interfaces2.ts
@@ -1,16 +1,20 @@
-export interface SearchAttributes {
+export interface SearchAttributes extends SAB {
+  referenceId?: string
+  referenceType?: string
+  ext?: any
+}
+interface SAA {
   id: string
   category: string
   content: string
   name: string
+}
+interface SAB extends SAA {
   createdAt: Date
   updatedAt: Date
   userId: string
   organizationId?: string
   workspaceId?: string
-  referenceId?: string
-  referenceType?: string
-  ext?: any
 }
 
 export interface Location {
